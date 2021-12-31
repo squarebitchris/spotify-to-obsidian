5800789fae3e874e72dc57d81e2c6b2714fd7fbb
squarebitchris <chris.wesley.allen@gmail.com>
2021-12-31 16:23:25 -0600

    working album querying and pagination

--- src/ui/STOPluginHOC.tsx
+++ src/ui/STOPluginHOC.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
-import { deconstructUrl, getTrack, getTrackFeatures, getAudioAnalysis, getArtist, getAlbum, buildNote, findBearerToken } from '../utils';
+import { deconstructUrl, getTrack, getTrackFeatures, getAudioAnalysis, getArtist, getAlbum, buildNote, findBearerToken, getPlaylist, getAllPlaylistTracks } from '../utils';
 
 export default function STOPluginHOC(): JSX.Element {
   // Input State
-  const [userInput, setUserInput] = React.useState("");
+  const [trackInput, setTrackInput] = React.useState("");
   const [trackId, setTrackId] = React.useState("");
+  const [playlistInput, setPlaylistInput] = React.useState("");
+  const [playlistId, setPlaylistId] = React.useState("");
   // Query state
   const [track, setTrack] = React.useState({});
   const [audioFeatures, setAudioFeatures] = React.useState({});
@@ -15,9 +17,19 @@ export default function STOPluginHOC(): JSX.Element {
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const createNoteHandler = async () => {
+    // Set loading state
+    setIsLoading(true);
+    // Create note
+    const note = await buildNote(track, artist, album, audioFeatures, audioAnalysis);
+    console.log('Note Methods', note);
+    setIsLoading(false);
+    return null;
+  }
+
+  const trackChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     // Update the user input state
-    setUserInput(e.target.value);
+    setTrackInput(e.target.value);
     // Validate user input for Spotify ID
     const spotifyID = deconstructUrl(e.target.value);
     if(spotifyID){
@@ -48,18 +60,8 @@ export default function STOPluginHOC(): JSX.Element {
     setArtist(artistData);
     setAlbum(albumData);
     // Reset input
-    setUserInput("");
+    setTrackInput("");
     setTrackId("");
-    setIsLoading(false);
-    return null;
-  }
-
-  const createNoteHandler = async () => {
-    // Set loading state
-    setIsLoading(true);
-    // Create note
-    const note = await buildNote(track, artist, album, audioFeatures, audioAnalysis);
-    console.log('Note Methods', note);
     setIsLoading(false);
     return null;
   }
@@ -89,19 +91,91 @@ export default function STOPluginHOC(): JSX.Element {
     }
   }
 
+  const playlistChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Update the user input state
+    setPlaylistInput(e.target.value);
+    // Validate user input for Spotify ID
+    const spotifyID = deconstructUrl(e.target.value);
+    if(spotifyID){
+      setPlaylistId(spotifyID);
+    } else {
+      setPlaylistId("...");
+    }
+  }
+
+  const searchPlaylistHandler = async () => {
+    // Set loading state
+    setIsLoading(true);
+    // Search Spotify for playlist information
+    // Acquire bearer token
+    const bearerToken = findBearerToken();
+
+    // Query Spotify API for playlist info
+    const playlistData = await getPlaylist(playlistId, bearerToken);
+    // const artistId = trackData.artists[0].id;
+    // const albumId = trackData.album.id;
+
+    // Query Spotify API for playlist tracks
+    // let playlistItems = [];
+    const trackCount = playlistData.tracks.total;
+    let playlistTracksData = await getAllPlaylistTracks(playlistId, trackCount, bearerToken);
+    // const audioFeaturesData = await getTrackFeatures(trackId, bearerToken);
+    // const audioAnalysisData = await getAudioAnalysis(trackId, bearerToken);
+
+    console.log('Playlist Data', playlistData);
+    
+
+    console.log('Playlist Items', playlistTracksData);
+
+    // TODO: Paginate playlist items if needed
+
+    // Update state
+
+    // const artistData = await getArtist(artistId, bearerToken);
+    // const albumData = await getAlbum(albumId, bearerToken);
+    // Update state
+    // setTrack(trackData);
+    // setAudioFeatures(audioFeaturesData);
+    // setAudioAnalysis(audioAnalysisData);
+    // setArtist(artistData);
+    // setAlbum(albumData);
+    // Reset input
+    // setUserInput("");
+    // setTrackId("");
+    setIsLoading(false);
+    return null;
+  }
+
+
   return (
     <>
+      {/* Track Query Form */}
       <div className="STOPluginHOC__container">
         <p>Paste Spotify Track Link</p>
-        <input onChange={changeHandler} value={userInput}></input>
+        <input onChange={trackChangeHandler} value={trackInput}></input>
       </div>
       <p>Track ID: </p>
       <h4>{trackId}</h4>
       <button onClick={searchTrackHandler}>
         Search for Track
       </button>
+      
+      {/* Playlist Query */}
+      <div className="STOPluginHOC__container">
+        <p>Paste Spotify Playlist Link</p>
+        <input onChange={playlistChangeHandler} value={playlistInput}></input>
+      </div>
+      <p>Playlist ID: </p>
+      <h4>{trackId}</h4>
+      <button onClick={searchPlaylistHandler}>
+        Search for Playlist
+      </button>
+
       {/* Card Component for Track Data */}
       {renderTrackInfo()}
+
+      {/* Card Component for Playlist Data */}
+
     </>
   );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,9 @@
 // src/utils.ts
 import { request } from 'obsidian';
 import { App, Notice, TFile } from "obsidian";
-
 // import { Raindrop, RaindropList } from './models';
 
-/**
- * Extracts a trackId from a Spotify URL
- * @param {string} url - The string that should be the correct url format -> "https://open.spotify.com/album/0BwWUstDMUbgq2NYONRqlu?si=5qGtCb3_Qh2SburnU09NtQ"
- * @returns {string} - Either the trackId or an empty string
- */
- export const deconstructUrl = (url: string): string => {
-  const regEx = /^(?:spotify:|(?:https?:\/\/(?:open|play)\.spotify\.com\/))(?:embed)?\/?(album|track)(?::|\/)((?:[0-9a-zA-Z]){22})/;
-  const match = url.match(regEx);
-  if(match){
-    const spotifyID = match[2]
-    return spotifyID
-  }
-  return "";
-}
+// Spotify API Resource Fucntions
 
 /**
  * Fetches a track object from the Spotify API
@@ -29,6 +15,42 @@ import { App, Notice, TFile } from "obsidian";
   const trackResponse = await requestAPI(`https://api.spotify.com/v1/tracks/${id}`, bearerToken);
   return trackResponse;
 }
+
+/**
+ * Fetches artist from the Spotify API
+ * @param {string} id - The artistId to fetch data from the Spotify API
+ * @param {string} bearerToken - The bearer token
+ * @returns {Artist} - The artist data from the Spotify API
+ */
+ export const getArtist = async (id: string, bearerToken: string): Promise<Artist> => {
+  const artistResponse = await requestAPI(`https://api.spotify.com/v1/artists/${id}`, bearerToken);
+  return artistResponse;
+}
+
+/**
+ * Fetches playlist from the Spotify API
+ * @param {string} id - The playlistId to fetch data from the Spotify API
+ * @param {string} bearerToken - The bearer token
+ * @returns {Playlist} - The playlist data from the Spotify API
+ */
+ export const getPlaylist = async (id: string, bearerToken: string): Promise<Playlist> => {
+  const playlistResponse = await requestAPI(`https://api.spotify.com/v1/playlists/${id}`, bearerToken);
+  return playlistResponse;
+}
+
+/**
+ * Fetches album from the Spotify API
+ * @param {string} id - The albumId to fetch data from the Spotify API
+ * @param {string} bearerToken - The bearer token
+ * @returns {Album} - The album data from the Spotify API
+ */
+ export const getAlbum = async (id: string, bearerToken: string): Promise<Album> => {
+  const albumResponse = await requestAPI(`https://api.spotify.com/v1/albums/${id}`, bearerToken);
+  return albumResponse;
+}
+
+
+// Track Metadata Functions
 
 /**
  * Fetches a track audio features from the Spotify API
@@ -52,55 +74,22 @@ import { App, Notice, TFile } from "obsidian";
   return audioAnalysisResponse;
 }
 
+// Playlist Metdata Functions
+// https://open.spotify.com/playlist/62XU0jZ2Zy95d5QQzhFGsq?si=ff104043591d41a5
+// https://open.spotify.com/playlist/15rSpQyYwSx3skNdXujx0T?si=65c73225b4ac41ac
+// Query => items(track(name,artists(name),album(name, images)))
 /**
- * Fetches artist from the Spotify API
- * @param {string} id - The artistId to fetch data from the Spotify API
+ * Fetches a playlists tracks from the Spotify API
+ * @param {string} id - The playlistId to fetch data from the Spotify API
  * @param {string} bearerToken - The bearer token
- * @returns {Artist} - The artist data from the Spotify API
+ * @returns {PlaylistTracks} - The track features from the Spotify API
  */
- export const getArtist = async (id: string, bearerToken: string): Promise<Artist> => {
-  const artistResponse = await requestAPI(`https://api.spotify.com/v1/artists/${id}`, bearerToken);
-  return artistResponse;
+export const getPlaylistTracks = async (id: string, bearerToken: string): Promise<PlaylistTracks> => {
+  const playlistTracksResponse = await requestAPI(`https://api.spotify.com/v1/playlists/${id}/tracks`, bearerToken);
+  return playlistTracksResponse;
 }
 
-/**
- * Fetches album from the Spotify API
- * @param {string} id - The albumId to fetch data from the Spotify API
- * @param {string} bearerToken - The bearer token
- * @returns {Album} - The album data from the Spotify API
- */
- export const getAlbum = async (id: string, bearerToken: string): Promise<Album> => {
-  const albumResponse = await requestAPI(`https://api.spotify.com/v1/albums/${id}`, bearerToken);
-  return albumResponse;
-}
-
-/**
- * Reusable function perform requests to the Spotify API
- * @param {string} url - The url to perform the request to
- * @param {string} bearerToken - The bearer token
- * @returns {Object} - The data from the Spotify API
- */
-const requestAPI = async (url: string, bearerToken: string): Promise<T> => {
-  const apiUrl = new URL(url);
-  let spotifyRequest;
-  try {
-    spotifyRequest = await request({
-      method: 'GET',
-      url: `${apiUrl.href}`,
-      headers: {Authorization: `Bearer ${bearerToken}`},
-    })
-  } catch (error) {
-    if (error.request) {
-      throw new Error('There seems to be a connection issue.')
-    } else {
-      console.error(error)
-      throw error
-    }
-  }
-  // console.log('response data', JSON.parse(spotifyRequest))
-  return JSON.parse(spotifyRequest);
-}
-
+// Obsidian Note Functions
 
 /**
  * Creates a Obsidian Note from Spotify data
@@ -265,7 +254,6 @@ const requestAPI = async (url: string, bearerToken: string): Promise<T> => {
   }
 }
 
-
 function titleCase(str) {
   const splitStr = str.toLowerCase().split(' ');
   for (let i = 0; i < splitStr.length; i++) {
@@ -276,6 +264,8 @@ function titleCase(str) {
   // Directly return the joined string
   return splitStr.join(' '); 
 }
+
+// Request Functions
 
 /**
  * Returns bearer token from plugin settings
@@ -288,3 +278,89 @@ function titleCase(str) {
   const bearerToken = pluginSettings.bearerToken || '';
   return bearerToken;
  }
+
+/**
+ * Extracts a trackId from a Spotify URL
+ * @param {string} url - The string that should be the correct url format -> "https://open.spotify.com/album/0BwWUstDMUbgq2NYONRqlu?si=5qGtCb3_Qh2SburnU09NtQ"
+ * @returns {string} - Either the trackId or an empty string
+ */
+ export const deconstructUrl = (url: string): string => {
+  const regEx = /^(?:spotify:|(?:https?:\/\/(?:open|play)\.spotify\.com\/))(?:embed)?\/?(album|track|playlist)(?::|\/)((?:[0-9a-zA-Z]){22})/;
+  const match = url.match(regEx);
+  if(match){
+    const spotifyID = match[2]
+    return spotifyID
+  }
+  return "";
+}
+
+/**
+ * Reusable function perform requests to the Spotify API
+ * @param {string} url - The url to perform the request to
+ * @param {string} bearerToken - The bearer token
+ * @returns {Object} - The data from the Spotify API
+ */
+const requestAPI = async (url: string, bearerToken: string): Promise<T> => {
+  const apiUrl = new URL(url);
+  let spotifyRequest;
+  try {
+    spotifyRequest = await request({
+      method: 'GET',
+      url: `${apiUrl.href}`,
+      headers: {Authorization: `Bearer ${bearerToken}`},
+    })
+  } catch (error) {
+    if (error.request) {
+      throw new Error('There seems to be a connection issue.')
+    } else {
+      console.error(error)
+      throw error
+    }
+  }
+  // console.log('response data', JSON.parse(spotifyRequest))
+  return JSON.parse(spotifyRequest);
+}
+
+
+/**
+ * Paginates through Tracks of a playlist
+ * @param {string} playlistId - The Spotify ID of the playlist
+ * @param {string} bearerToken - The bearer token
+ * @returns {Array} - An array of track objects
+ * @see https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlist-tracks/
+ */
+export const getAllPlaylistTracks = async (playlistId: string, trackCount: number,  bearerToken: string): Promise<Array<any>> => {
+  // Get the first page of tracks
+  let flag = true;
+  // let counterBlock = 0;
+  const tracksArray = [];
+  const query_obj = {limit: 20, offset: 0, parallel: Math.ceil(trackCount/20)};
+  const paginateUrl = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=items(track(name%2Cartists(name)%2Calbum(name%2C%20images)))`;
+  while (flag) {
+    // Create Batched Request
+    const waitAll = range(query_obj.parallel).map((idx) => 
+      requestAPI(`${paginateUrl}&limit=${query_obj.limit}&offset=${(query_obj.offset + idx*query_obj.limit)}`, bearerToken)
+    );
+
+    await Promise.all(waitAll).then(result => {
+      // Iterate through parallel requests
+      result.map(r => {
+        // Add tracks to return array
+        r.items.forEach(function(element) { 
+          tracksArray.push(element);
+        });
+      })
+      // Need to find if any of the results contain no items
+      flag = result.every(r => r.items.length > 0)
+      // Update offset
+      query_obj.offset += query_obj.limit * query_obj.parallel;
+    });
+  }
+  return tracksArray;
+}
+
+function range(size, startAt = 0) {
+  return [...Array(size).keys()].map(i => i + startAt);
+}
+
+
